da5b2a82ea4b42e4c84267ef9e6390a40068a4e8
Neil Parikh <parikh.neil@me.com>
2024-12-02 22:37:19 -0800

    let -> where

--- day2.hs
+++ day2.hs
@@ -13,7 +13,9 @@ part2 = length . filter (any safe) . fmap withOneDropped
     withOneDropped xs = fmap (without xs) [0..(length xs - 1)]
 
 safe :: [Int] -> Bool
-safe xs = let diffs = zipWith subtract xs (tail xs) in (all (> 0) diffs || all (< 0) diffs) && all ((<= 3) . abs) diffs
+safe xs = (all (> 0) diffs || all (< 0) diffs) && all ((<= 3) . abs) diffs
+    where
+    diffs = zipWith subtract xs (tail xs)
 
 without :: [Int] -> Int -> [Int]
 without xs i = take i xs ++ drop (i + 1) xs
